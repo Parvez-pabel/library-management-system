2637b6e39de2eee0874c8bd778911554e8129da0
Parvez-pabel <pabelalomparvez2233@gmail.com>
2026-07-29 00:04:29 +0600

    updated all auth functions

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import validator from "validator";
+import { hashPassword, matchPassword } from "../utils/passwordHash.js";
 
 const UserSchema = new mongoose.Schema(
   {
@@ -10,16 +12,51 @@ const UserSchema = new mongoose.Schema(
       lowercase: true,
       validate: [validator.isEmail, "Please provide a valid email address"],
     },
-    password: { type: String, required: true },
+    password: {
+      type: String,
+      required: true,
+      minlength: [8, "Password must be at least 6 characters long"],
+    },
     phone: { type: String, required: true },
     role: {
       type: String,
       required: true,
-      Enum: ["Admin", "User"],
-      Default: "User",
+      enum: ["Admin", "User"],
+      default: "User",
+    },
+    photo: {
+      type: String,
+    },
+    otp: {
+      type: String,
+      default: "0",
+    },
+    otpExpiry: {
+      type: Date,
+      default: null,
+    },
+    status: {
+      type: String,
+      default: "unverified",
     },
   },
   { timestamps: true },
 );
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    this.password = await hashPassword(this.password);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+UserSchema.methods.comparePassword = async function (userPassword) {
+  return await matchPassword(userPassword, this.password);
+};
 
 export default mongoose.model("Users", UserSchema);
